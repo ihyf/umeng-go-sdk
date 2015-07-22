641aeeec66ec21d9c1c961a3bdd037d7c89f9534
ZhiFeng Hu <hzfeng@vip.deyi.com>
2015-07-22 08:57:04 +0800

    Define struct for android notification

--- android_unicast.go
+++ android_unicast.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+type AndroidPayloadBody struct {
+	Ticker  string `json:"ticker"`
+	Title string `json:"title"`
+	Text string `json:"text"`
+	Play_vibrate string `json:"play_vibrate"`
+	Play_lights string `json:"play_lights"`
+	Play_sound string  `json:"play_sound"`
+	After_open string `json:"after_open"`
+}
+type AndroidPayload struct {
+	Display_type string `json:"display_type"`
+	Body	androidpayloadbody `json:"body"`
+
+}
 func Version() {
 	fmt.Println("0.0.1")
 }
