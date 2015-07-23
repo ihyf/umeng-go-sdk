6dcbc3da6161d742cdea9e94e27062c79f3a31f4
ZhiFeng Hu <hzfeng@vip.deyi.com>
2015-07-23 21:39:48 +0800

    Add keys define const and global vars

---
+++ keys.go
@@ -0,0 +1,29 @@
+package umeng
+
+const (
+	DATA_KEYS = []string{
+		"appkey",
+		"timestamp",
+		"type",
+		"device_tokens",
+		"alias",
+		"alias_type",
+		"file_id",
+		"filter",
+		"production_model",
+		"feedback",
+		"description",
+		"thirdparty_id",
+	}
+	POLICY_KEYS = []string{
+		"start_time",
+		"expire_time",
+		"max_send_num",
+	}
+)
+var (
+	Host string = "http://msg.umeng.com"
+	UploadPath string = "/upload"
+	PostPath string = "/api/send"
+	AppMasterSecret string
+)
