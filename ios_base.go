02fbdfbd6f78be000c6fb5659e6bd8975ed70666
ZhiFeng Hu <hzfeng@vip.deyi.com>
2015-07-24 13:46:47 +0800

    Define iospayload

--- ios_base.go
+++ ios_base.go
@@ -8,5 +8,15 @@ type IOSPayloadAPS struct {
 	Category string `json:"category"`
 }
 type IOSPayload struct {
-	APS IOSPayloadAPS `json:"aps"`
+	Alert string `json:"alert"`
+	badge string `json:"badge"`
+	sound string `json:"sound"`
+	ContentAvailable string `json:"content-available"`
+	Category string `json:"category"`
+	CustomFields map[string]interface{}
+}
+
+func (p *IOSPayload) marshalPayload() ([]byte, error) {
+	var jsonStr []byte
+	return jsonStr, nil
 }
